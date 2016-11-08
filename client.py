3b8024c42da4fb86332ea6be02ab2fbf09a3c073
Luca Zenobi <l.zenobi@t4project.com>
2016-11-08 09:54:21 +0100

    Implemented download socket input to file

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from sys import stdin
 from sys import stdout
 
 HOST = 'localhost'
-PORT = 4025
+PORT = 50025
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.connect((HOST, PORT))
@@ -14,9 +14,4 @@ stdout.write('Send message: ')
 stdout.flush()
 
 soc.sendall(stdin.readline())
-
-data = soc.recv(1024)
-if data:
-    stdout.write('Message received: ' + data)
-
 soc.close()
--- server.py
+++ server.py
@@ -1,19 +1,37 @@
 # Echo server program
+import os
 import socket
+from datetime import datetime
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 4025              # Arbitrary non-privileged port
+HOST = ""                 # Symbolic name meaning all available interfaces
+PORT = 50025              # Arbitrary non-privileged port
+DOWNLOAD_DIR = "downloads"
+
+def writeToFileConnectionData(connection, filename):
+    file = open(filename, "w")
+    print "File opened:", filename
+
+    while 1:
+        data = connection.recv(1024)
+        if not data: break
+        file.write(data)
+
+    file.close()
+    print "File closed:", filename
+
+def createDownloadDir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+createDownloadDir(DOWNLOAD_DIR)
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 soc.bind((HOST, PORT))
-soc.listen(1)
-
-conn, addr = soc.accept()
-print 'Connected by', addr
+soc.listen(5)
 
 while 1:
-    data = conn.recv(1024)
-    if not data: break
-    conn.sendall(data)
-
-conn.close()
+    conn, addr = soc.accept()
+    print "Connected by:", addr
+    writeToFileConnectionData(conn, DOWNLOAD_DIR + "/" + datetime.now().strftime("%Y%m%d%H%M%S") + ".txt")
+    conn.close()
+    print "Coonection closed:", addr
